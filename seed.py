63b043e8ccc95b4f8890202a053f070ce8f05514
suhaimaislam <73371083+suhaimaislam@users.noreply.github.com>
2022-03-07 23:49:28 -0500

    Update seed.py

--- seed.py
+++ seed.py
@@ -3,15 +3,42 @@ import datetime
 
 def seed(db):
     employees = [
-
+        Employees(id=1, username="George Washington"),
+        Employees(id=2, username="Thomas Jefferson"),
+        Employees(id=3, username="Benjamin Franklin"),
+        Employees(id=4, username="Abraham Lincoln"),
+        Employees(id=5, username="John Adams"),
+        Employees(id=6, username="Alexander Hamilton"),
+        Employees(id=7, username="Aaron Burr"),
+        Employees(id=8, username="James Madison"),
+        Employees(id=9, username="James Monroe"),
+        Employees(id=10, username="John Jay"),
     ]
 
     customers = [
-
+        Customers(id=1, username="Mark Twain"),
+        Customers(id=2, username="Charles Dickens"),
+        Customers(id=3, username="Walt Whitman"),
+        Customers(id=4, username="Francis Fitzgerald"),
+        Customers(id=5, username="John Steinbeck"),
+        Customers(id=6, username="Edgar Allen Poe"),
+        Customers(id=7, username="Louisa Alcott"),
+        Customers(id=8, username="Agatha Christie"),
+        Customers(id=9, username="Jane Austen"),
+        Customers(id=10, username="Charlotte Bronte"),
     ]
     
     visitors = [
-
+        Visitors(id=1, username="Brad Pitt"),
+        Visitors(id=2, username="Adam Sandler"),
+        Visitors(id=3, username="Bradley Cooper"),
+        Visitors(id=4, username="Leonardo DiCaprio"),
+        Visitors(id=5, username="Tom Hanks"),
+        Visitors(id=6, username="Bruce Willis"),
+        Visitors(id=7, username="George Clooney"),
+        Visitors(id=8, username="Rob Lowe"),
+        Visitors(id=9, username="Ben Affleck"),
+        Visitors(id=10, username="Alec Baldwin"),
     ]
 
     db.session.add_all(employees)
